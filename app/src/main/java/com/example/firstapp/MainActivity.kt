f483c4dcf8b81e83408de597d26fac6d45179d30
Dago Peralta <dagoperalta163@gmail.com>
2022-05-13 06:19:41 -0500

    Agregando segunda actividad de recorrido (Boton de Navegación)

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.firstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.firstapp.databinding.ActivityMainBinding
@@ -12,10 +13,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.goToActivity1.setOnClickListener { goingToActivity1() }
+        binding.goToActivity2.setOnClickListener { goingToActivity2() }
+
+    }
+    private fun goingToActivity1(){
+        val intent = Intent(this, MainActivity2::class.java).apply {
+
+        }
+        startActivity(intent)
+    }
+    private fun goingToActivity2(){
+        val intent = Intent(this, MainActivity3::class.java).apply {
+
+        }
+        startActivity(intent)
     }
 }
 
-class goToActivity1() {
 
-}
 
