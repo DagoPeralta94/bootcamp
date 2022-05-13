7ddedae3b73e614b1a42de52173c21059d29bbed
Dago Peralta <dagoperalta163@gmail.com>
2022-05-13 06:24:10 -0500

    Agregando cuarta actividad de recorrido (FullScreen)

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         binding.goToActivity1.setOnClickListener { goingToActivity1() }
         binding.goToActivity2.setOnClickListener { goingToActivity2() }
         binding.goToActivity3.setOnClickListener { goingToActivity3() }
+        binding.goToActivity4.setOnClickListener { goingToActivity4() }
 
     }
     private fun goingToActivity1(){
@@ -33,6 +34,12 @@ class MainActivity : AppCompatActivity() {
     }
     private fun goingToActivity3(){
         val intent = Intent(this, MainActivity4::class.java).apply {
+
+        }
+        startActivity(intent)
+    }
+    private fun goingToActivity4(){
+        val intent = Intent(this, FullscreenActivity::class.java).apply {
 
         }
         startActivity(intent)
